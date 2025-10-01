396de26a07254982a382e64dfd528cdd9130483b
Ppaint <narudee_sornsawat@cmu.ac.th>
2025-10-01 19:21:05 +0700

    finish

---
+++ src/middlewares/checkRoleMiddleware.ts
@@ -0,0 +1,31 @@
+
+import { type Response, type NextFunction } from "express";
+import { type CustomRequest, type User } from "../libs/types.js";
+import { users } from "../db/db.js";
+
+export const checkRole = (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const payload = req.user;
+  const token = req.token;
+
+  const user = users.find((u: User) => u.username === payload?.username);
+  if (!user) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized user",
+    });
+  }
+
+
+  if (user.tokens && token && !user.tokens.includes(token)) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+
+  return next();
+};
